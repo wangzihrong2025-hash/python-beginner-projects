5e46befd1d76807f98aac8dbb3fefe7422e0a55e
ワンツロン <wangzihrong2025@gmail.com>
2025-12-16 00:20:05 +0900

    Update day01_variable.py

--- day01_variable.py
+++ day01_variable.py
@@ -1,9 +1,23 @@
-python
-
-
+"""
+ver.01
 name="Alex"
 age=25
 
 print(name)
 print(age)
 print("My name is",name+",I'm",age,"years old")
+"""
+#ver2
+#name="Alex"
+#age=25
+
+#print(name)
+#print(age)
+#print(f"my name is {name}, I'm {age} years old")
+
+
+# ver 0.3 (input)
+name = input("Name: ")
+age = int(input("Age: "))
+
+print(f"My name is {name}, I'm {age} years old.")
