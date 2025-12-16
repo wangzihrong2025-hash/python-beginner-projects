d7b20b862f80c06d9a89565a079c471f80f145fe
ワンツロン <wangzihrong2025@gmail.com>
2025-12-16 23:45:10 +0900

    Update day02_if_else.py

--- day02_if_else.py
+++ day02_if_else.py
@@ -6,12 +6,27 @@
 #  else
  # print("minor")
 
-
+#--------------------------
 #python(if_else_ver.02)
-name=input("Name :")
-age= int(input("Age :"))
+#name=input("Name :")
+#age= int(input("Age :"))
 
-if age>=18:
- print(f"{name}is a adlt.")
-else
-print(f"{name}is a minor.")
+#if age>=18:
+# print(f"{name}is a adlt.")
+#else
+#print(f"{name}is a minor.")
+#--------------------------
+#python(if_else_ver.03)
+
+name=input("Name :")
+age=int(input("Age :"))
+
+if age >= 18:
+print(f"{name} is a adlt.")
+else if age >= 13:
+print(f"{name} is a teenager")
+else:
+print(f"{name} is a child")
+
+
+
