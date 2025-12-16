010903ee4fa55f8f5c659b8e75fd84a270dccb87
ワンツロン <wangzihrong2025@gmail.com>
2025-12-16 23:33:38 +0900

    Update and rename day02_if_else to day02_if_else.py

---
+++ day02_if_else.py
@@ -0,0 +1,17 @@
+#python(if_else_ver.01)
+#age=20
+
+# if age >= 18
+#  print("adlt")
+#  else
+ # print("minor")
+
+
+#python(if_else_ver.02)
+name=input("Name :")
+age= int(input("Age :"))
+
+if age>=18:
+ print(f"{name}is a adlt.")
+else
+print(f"{name}is a minor.")
