35a39e5f7d05b8a45763cf3aa466ec379341a120
ワンツロン <wangzihrong2025@gmail.com>
2025-12-18 00:23:02 +0900

    Update day03_for.py

--- day03_for.py
+++ day03_for.py
@@ -1,2 +1,20 @@
+#for#for i in range(
+ #    print("Hellow",i)
+#-----------------------------------
+#total = 0
+
+#for i in range(3):
+#    num = int(input("Enter a number : "))
+#    total = total + num
+
+#print("Total : ",total)
+#-----------------------------------------
+
+
 for i in range(3):
-     print("Hellow")
+     age = int(input("Age : "))
+     if age >=18
+         print("Adult")
+     else
+         print("Minor")
+     
