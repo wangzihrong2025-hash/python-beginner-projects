e41d401a79120404355e8a0b7529c2d5747a6228
ワンツロン <wangzihrong2025@gmail.com>
2025-12-24 23:18:36 +0900

    Update and rename day4_practice_for to day4_practice_for_01.py

---
+++ day4_practice_for_01.py
@@ -0,0 +1,15 @@
+#python case_01
+
+ for i in range(5):
+ age = int(input("Age:"))
+ name = input("Name:")
+   if age >=18:
+       print(f"{name} is an adult.")
+   elif age>=13:
+       print(f"{name} is a teenager.")
+   else:
+       print(f"{name} is a child.")
+
+
+
+
