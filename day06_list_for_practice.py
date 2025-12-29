9397238c3ab94a03f116213e9d835c54b09549b0
ワンツロン <wangzihrong2025@gmail.com>
2025-12-29 21:41:19 +0900

    Update day06_list_for_practice.py

--- day06_list_for_practice.py
+++ day06_list_for_practice.py
@@ -1,1 +1,13 @@
+#python
 
+ages = []
+for i in range(5):
+  age = int(input(f"Age : #No.{i+1}"))
+  ages.append(age)
+for age in ages:
+  if age >= 18:
+    print("adult")
+  elif age >=12:
+    print("teenager")
+  else :
+    print("child")
